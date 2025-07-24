0d84ca8252ea6c15cc204fcd3ff304f02b7df2e5
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2025-07-24 18:21:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from manim import *
+
+
+class ParafieldStudios(Scene):
+    def construct(self):
+        text = Text("PARAFIELD \n STUDIOS", font="Segoe UI", weight="BOLD")
+        logo = SVGMobject("parafield.svg")
+        self.play(FadeIn(logo), run_time=1.5)
+        self.play(
+            AnimationGroup(
+                logo.animate.next_to(text, LEFT), Write(text), lag_ratio=0.8
+            ),
+            run_time=3,
+        )
+        self.wait(2)
